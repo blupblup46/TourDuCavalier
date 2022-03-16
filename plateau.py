99b57f742bd04534ea22ec7e967e18b4ff844863
Trykeur <97159800+Trykeur@users.noreply.github.com>
2022-03-16 11:19:25 +0100

    Add files via upload

--- plateau.py
+++ plateau.py
@@ -1,46 +1,51 @@
 import networkx as nx 
 import matplotlib.pyplot as plt
 import mygraph
+import random
 
-def plateauCarre(n):
+def grapheplateauCarre(n):
 
-    echequier = dict()
+    echiquier = dict()
 
     for k in range(n*n): # n*n -> dimentions de l'échequier
         i = k//n         #ligne de la case k
         j = k%n          #colone de la case k
-        echequier[k]=[]  #liste des voisins de la case k
+        echiquier[k]=[]  #liste des voisins de la case k
 
         if  (0<=i-2<n) and (0<=j-1<n) : 
-            echequier[k].append((i-2)*n+(j-1))
+            echiquier[k].append((i-2)*n+(j-1))
 
-        if  (0<=i-2<n) and  (0<=j+1<n) : 
-            echequier[k].append((i-2)*n+(j+1))
+        if  (0<=i-2<n) and (0<=j+1<n) : 
+            echiquier[k].append((i-2)*n+(j+1))
 
-        if  (0<=i-1<n) and  (0<=j-2<n) : 
-            echequier[k].append((i-1)*n+(j-2))
+        if  (0<=i-1<n) and (0<=j-2<n) : 
+            echiquier[k].append((i-1)*n+(j-2))
 
-        if  (0<=i-1<n) and  (0 <=j+2<n) : 
-            echequier[k].append((i-1)*n+(j+2))
+        if  (0<=i-1<n) and (0 <=j+2<n) : 
+            echiquier[k].append((i-1)*n+(j+2))
 
-        if  (0<=i+1<n) and  (0<=j-2<n) : 
-            echequier[k].append((i+1)*n+(j-2))
+        if  (0<=i+1<n) and (0<=j-2<n) : 
+            echiquier[k].append((i+1)*n+(j-2))
 
-        if  (0<=i+1<n) and  (0<=j+2<n) : 
-            echequier[k].append((i+1)*n+(j+2))
+        if  (0<=i+1<n) and (0<=j+2<n) : 
+            echiquier[k].append((i+1)*n+(j+2))
 
-        if  (0<=i+2<n) and  (0<=j-1<n) : 
-            echequier[k].append((i+2)*n+(j-1))
+        if  (0<=i+2<n) and (0<=j-1<n) : 
+            echiquier[k].append((i+2)*n+(j-1))
 
-        if  (0<=i+2<n) and  (0<=j+1<n) : 
-            echequier[k].append((i+2)*n+(j+1))
+        if  (0<=i+2<n) and (0<=j+1<n) : 
+            echiquier[k].append((i+2)*n+(j+1))
     
-    return echequier
+    return echiquier
 
+    
 
-graphe = plateauCarre(4)
-affichage = nx.Graph()
-affichage.add_nodes_from(mygraph.Graphe(graphe).all_sommets())
-affichage.add_edges_from(mygraph.Graphe(graphe).all_aretes())
-nx.draw(affichage,with_labels=True,font_weight="bold")
-plt.show()
+def affichageCarre(n):
+    graphe = grapheplateauCarre(n)
+    affichage = nx.Graph()
+    affichage.add_nodes_from(mygraph.Graphe(graphe).all_sommets())
+    affichage.add_edges_from(mygraph.Graphe(graphe).all_aretes())
+    nx.draw(affichage,with_labels=True,font_weight="bold")
+    plt.show()
+
+affichageCarre(4)
