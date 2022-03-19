f8c58815ff6bb8471e2e02cb5cae33743ce76049
Allan <97159800+Trykeur@users.noreply.github.com>
2022-03-19 16:37:29 +0100

    Add files via upload

--- plateau.py
+++ plateau.py
@@ -46,45 +46,45 @@ def affichageCarre(n):
     plt.show()
 
 
-def graphePlateauRectangle(larg,long):
+def graphePlateauRectangle(nbColone,nbLigne):
     echiquier = dict()
-    for k in range(larg*long):
-        ligne=k//larg
-        colone=k%larg
+    for k in range(nbColone*nbLigne):
+        ligne=k//nbColone
+        colone=k%nbColone
         echiquier[k]=[]
     
-        if (0<=ligne-2<long)and(0<=colone-1<larg):
-            echiquier[k].append((ligne-2)*larg+(colone-1))
+        if (0<=ligne-2<nbLigne)and(0<=colone-1<nbColone):
+            echiquier[k].append((ligne-2)*nbColone+(colone-1))
 
-        if (0<=ligne-1<long)and(0<=colone-2<larg):
-            echiquier[k].append((ligne-1)*larg+(colone-2))
+        if (0<=ligne-1<nbLigne)and(0<=colone-2<nbColone):
+            echiquier[k].append((ligne-1)*nbColone+(colone-2))
 
-        if (0<=ligne-2<long)and(0<=colone+1<larg):
-            echiquier[k].append((ligne-2)*larg+(colone+1))
+        if (0<=ligne-2<nbLigne)and(0<=colone+1<nbColone):
+            echiquier[k].append((ligne-2)*nbColone+(colone+1))
 
-        if (0<=ligne-1<long)and(0<=colone+2<larg):
-            echiquier[k].append((ligne-1)*larg+(colone+2))
+        if (0<=ligne-1<nbLigne)and(0<=colone+2<nbColone):
+            echiquier[k].append((ligne-1)*nbColone+(colone+2))
 
-        if (0<=ligne+1<long)and(0<=colone-2<larg):
-            echiquier[k].append((ligne+1)*larg+(colone-2))
+        if (0<=ligne+1<nbLigne)and(0<=colone-2<nbColone):
+            echiquier[k].append((ligne+1)*nbColone+(colone-2))
 
-        if (0<=ligne+2<long)and(0<=colone-1<larg):
-            echiquier[k].append((ligne+2)*larg+(colone-1))
+        if (0<=ligne+2<nbLigne)and(0<=colone-1<nbColone):
+            echiquier[k].append((ligne+2)*nbColone+(colone-1))
 
-        if (0<=ligne+1<long)and(0<=colone+2<larg):
-            echiquier[k].append((ligne+1)*larg+(colone+2))
+        if (0<=ligne+1<nbLigne)and(0<=colone+2<nbColone):
+            echiquier[k].append((ligne+1)*nbColone+(colone+2))
         
-        if (0<=ligne+2<long)and(0<=colone+1<larg):
-            echiquier[k].append((ligne+2)*larg+(colone+1))
+        if (0<=ligne+2<nbLigne)and(0<=colone+1<nbColone):
+            echiquier[k].append((ligne+2)*nbColone+(colone+1))
     
     return echiquier
 
-def affichageRectangle(n,m):
-    graphe = graphePlateauRectangle(n,m)
+def affichageRectangle(nbColone,nbLigne):
+    graphe = graphePlateauRectangle(nbColone,nbLigne)
     affichage = nx.Graph()
     affichage.add_nodes_from(mygraph.Graphe(graphe).all_sommets())
     affichage.add_edges_from(mygraph.Graphe(graphe).all_aretes())
     nx.draw(affichage,with_labels=True,font_weight="bold")
     plt.show()
 
-affichageRectangle(4,5)
+affichageRectangle(2,3)
