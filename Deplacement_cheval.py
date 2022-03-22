f5fd2be18955aaef03f26b66d012a51939591a2c
Louan <90987010+blupblup46@users.noreply.github.com>
2022-03-22 17:33:22 +0100

    Le cheval c'est trop génial

--- Deplacement_cheval.py
+++ Deplacement_cheval.py
@@ -39,52 +39,51 @@ from collections import deque
 
 ##Initialisation du dictionnaire des distances
 
-def dfs_cavalierEdition (depart, depart_statique, passe): 
+def dfs_cavalierEdition (depart): 
     
     if depart not in visite:
         
-        if (passe != 0):
-            visite.append(depart)
-            Q.pop()
+        visite.append(depart)
+        Q.pop()
             
         print(visite)
-
+        
+        voisinTrie = list()
+        
+        for boucle in range (len(graph[depart])):
+            minPosibilite= 999
+            minSommet = 1
+            for voisin in graph[depart]:
                 
-        for voisin in graph[depart]:
+                
+                if (voisin not in voisinTrie):
+                    if (minPosibilite > len(graph[voisin])):
+                        minPosibilite = len(graph[voisin])
+                        minSommet = voisin
             
+            voisinTrie.append(minSommet)
+                
+        for voisin in voisinTrie:
             if (voisin not in visite):
-                
-                
-                
-                #print(graph[voisin])
-                dfs_cavalierEdition(voisin, depart_statique, passe+1)
+                    #print(graph[voisin])
+                    dfs_cavalierEdition(voisin)
        
         if (len(Q)!=0 ):
             visite.pop()
             Q.append(1)
         
-        
-        elif (len(Q)==0 and visite[-1]!= depart_statique):
-            visite.pop()
-            Q.append(1)
-        
-        
-   
-            
-        
 
 graph = plateauCarre(6)
 visite=[]
 Q=[]
-DEPART = 5
+DEPART = 0
 
 print("recherche...")
 for sommet in graph.keys():
     Q.append(sommet)
     
     
-dfs_cavalierEdition(DEPART, DEPART, 0)
-visite.insert(0, DEPART)
+dfs_cavalierEdition(DEPART)
 
 print("chemin = ")
 print(visite)
